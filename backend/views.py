bde92e048c48a44f2843b3a79616ece8115da8ba
Michał Dębosz <michal.debosz@arcelormittal.com>
2020-01-10 14:28:26 +0100

    Add Json

--- backend/views.py
+++ backend/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+import json
 
 def index(request):
     return HttpResponse("Main Page")
 
 def tent(request, country_name, city_name, tentID):
-    return HttpResponse("Tent Site Page")
+    # some JSON:
+    x = '{ "countryName":' +country_name+', "cityName":'+city_name+', "tentID":'+str(tentID)+'}'
+    return HttpResponse("Tent Site Page {}:{}:{} /n {}".format(country_name, city_name, tentID, json.loads(x)))
