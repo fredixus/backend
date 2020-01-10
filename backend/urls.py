169d6b400c40ce2ecb9960c60340630f120daf37
Michał Dębosz <michal.debosz@arcelormittal.com>
2020-01-10 14:27:17 +0100

    A Json

--- backend/urls.py
+++ backend/urls.py
@@ -23,6 +23,5 @@ urlpatterns = [
     path('', views.index),
     path('api/', include('api.urls')),
 
-    #ex.: /api/get/poland/cracow/5
-    path('api/get/<str:country_name>/<str:city_name>/<int:tentID>', views.tent, name='tent')
+
 ]
