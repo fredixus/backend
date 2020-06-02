da5638d04538b85560a7a2260a3eaaa38c3c8c3c
Jędrzej Krawczyk <siewierjedr@gmail.com>
2020-06-02 12:29:47 +0200

    create Login view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Login from './views/Login/Login'
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Michal's and Jedrzej's TentApp.</p>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+import {
+    BrowserRouter as Router,
+    Route,
+} from 'react-router-dom'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <Router>
+                    <Route path="/login" component={Login} />
+                </Router>
+            </div >
+        );
+    }
 }
 
 export default App;
