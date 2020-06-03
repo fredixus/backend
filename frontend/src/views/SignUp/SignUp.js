adf9667efd5a1c4d31d6caf94b24e21633290a31
Jędrzej Krawczyk <siewierjedr@gmail.com>
2020-06-03 09:56:06 +0200

    Add Styles for Signup View

--- frontend/src/views/SignUp/SignUp.js
+++ frontend/src/views/SignUp/SignUp.js
@@ -6,26 +6,34 @@ import {
 
 class SignUp extends React.Component {
     state = {
-        
+
     }
 
     render() {
         return (
             <div className="SignUp">
                 <div className="signup-card">
-                    <header className="header">
-                        SIGN IN
-                    </header>
+                    <header className="header">SIGN UP</header>
                     <form>
-                        <input name="name" type="text" placeholder="Your Name" required />
-                        <input name="surname" type="text" placeholder="Your Surname" required />
+                        <div>
+                            <input name="name" type="text" placeholder="Your Name" required />
+                            <input name="surname" type="text" placeholder="Your Surname" required />
+
+                        </div>
+
                         <input name="country" type="text" placeholder="Choose a Country" required />
                         {/* Country input is to changed on select type */}
                         <input name="email" type="email" placeholder="Email" required />
-                        <input name="login" type="text" placeholder="Your Login" required />
-                        <input name="password" type="password" placeholder="Your Password" required />
+                        <div>
+                            <input name="login" type="text" placeholder="Your Login" required />
+                            <input name="password" type="password" placeholder="Your Password" required />
+                        </div>
                     </form>
-                    <button>JOIN US</button>
+
+                    <Link to="/login">
+                        <button>JOIN US</button>
+                    </Link>
+
                 </div>
             </div>
         );
